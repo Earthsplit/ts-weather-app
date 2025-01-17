d1907d3d8b2bb017e80a9dd6771f7e837b09710f
Earthsplit <codingmonkey148@gmail.com>
2025-01-17 15:43:33 +0100

    add: debounce input change

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import { IOptionItem } from '../option.interface'
 import { IWeatherList } from '../components/Weather/Weather.interface'
 
+const DEBOUNCE_TIME = 750
+
 const useWeather = () => {
-	const [city, setCity] = useState<string>('')
+	const [city, setCity] = useState('')
+	const [debouncedCity, setDebouncedCity] = useState('')
 	const [options, setOptions] = useState<[]>([])
 	const [weather, setWeather] = useState<IWeatherList | null>(null)
 
-	const getOptions = async (value: string) => {
+	const fetchOptions = async (value: string) => {
+		if (!value) return
 		const response = await fetch(
 			`https://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=5&appid=${
 				import.meta.env.VITE_APP_API_KEY
@@ -17,17 +21,31 @@ const useWeather = () => {
 		setOptions(data)
 	}
 
+	useEffect(() => {
+		const handler = setTimeout(() => {
+			setDebouncedCity(city)
+		}, DEBOUNCE_TIME)
+
+		return () => {
+			clearTimeout(handler)
+		}
+	}, [city])
+
+	useEffect(() => {
+		if (debouncedCity) {
+			fetchOptions(debouncedCity)
+		} else {
+			setOptions([])
+		}
+	}, [debouncedCity])
+
 	const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const value = e.target.value
+		setCity(value)
 		if (value === '') {
-			setCity('')
 			setOptions([])
 			setWeather(null)
-			return
 		}
-
-		setCity(value)
-		getOptions(value)
 	}
 
 	const onOptionSelect = async (option: IOptionItem) => {
